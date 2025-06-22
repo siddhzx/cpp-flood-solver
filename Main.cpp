3e44e977e2cb98b7566cb2079d99efef01a54328
sidd singh <singhsiddhantakshat@gmail.com>
2025-06-21 19:08:37 -0500

    quick commit: brainstormed ideas + stride access pattern for element access

--- Main.cpp
+++ Main.cpp
@@ -4,24 +4,37 @@
 #include <random>
 #include <string>
 #include <algorithm>
+#include <set>
+#include <queue>
 
 #define l long
 #define ll long long
+#define LLMAX 9223372036854775807LL
 #define DEBUG 
+#define TEST
 
 struct CoorSystem {
     std::vector<ll> dims;
     std::vector<ll> board;
+    ll colors;
+    ll get(std::vector<ll>);
 };
+
+ll CoorSystem::get(std::vector<ll> coordinates) {
+    /* stride access pattern */
+    if (coordinates.empty() || coordinates.size() > dims.size()) return board[0];
+    
+}
 
 /* helpers */
 // @todo format better in the future
 void print_board(std::vector<ll> board) {
     // @todo - save to txt file for use in javascript / Three.js visualizations / useful for web (integrate this C++ code onto a web app)
-    ll bufferSize = 10;
+    ll bufferSize = 15;
     for (int i = 0; i < board.size(); ++i) {
         std::cout << board[i];
-        if ((i + 1) % bufferSize == 0 || i == board.size() - 1) std::cout << std::endl;
+        if (i == board.size() - 1) std::cout << std::endl;
+        else if ((i + 1) % bufferSize == 0 ) std::cout << ", " << std::endl;
         else std::cout << ", ";   
     }
 }
@@ -57,6 +70,14 @@ std::vector<ll> gen_rand_board(ll colors, ll size) {
     print_board(ret);
     #endif
 
+    return ret;
+}
+
+std::vector<ll> gen_increasing_board(ll colors, ll size) {
+    std::vector<ll> ret;
+    for (ll i = 0; i < size; ++i) {
+        ret.push_back(i);
+    }
     return ret;
 }
 
@@ -109,7 +130,12 @@ CoorSystem init_board(CoorSystem System, std::vector<ll> dims, std::tuple<bool, 
     }    
     // accessing undefined memory led to segfault bruh.
     auto [isRandomInit, colors, initState] = ifRandElseState;
+    System.colors = colors;
     if (isRandomInit) {
+        #ifdef TEST
+        System.board = gen_increasing_board(colors, size);
+        return System;
+        #endif
         System.board = gen_rand_board(colors, size);
     }
     else {
@@ -124,6 +150,55 @@ CoorSystem init_board(CoorSystem System, std::vector<ll> dims, std::tuple<bool, 
     return System;
 }
 
+/* solver */
+ll solver(CoorSystem System) {
+    /* by default the goal is to get from 0,0,...,0 to n,n,...,n where n = dim.size()*/
+    std::vector<ll> board = System.board;
+    ll colors = System.colors;
+
+    // assume 2d setup
+    ll rows = System.dims[0];
+    ll cols = System.dims[1];
+    std::tuple<ll, ll> curr = std::tuple{0LL, 0LL};
+    std::set<std::tuple<ll, ll>> visited;
+    std::priority_queue<std::tuple<ll, ll, ll>> pq;
+    pq.push(std::make_tuple(LLMAX, 0, 0));
+    std::vector<std::tuple<int, int>> directions = {std::tuple{1, 1}, std::tuple{0, 1}, std::tuple{1, 0}}; 
+
+    while (!pq.empty()) {
+        auto [price, x, y] = pq.top();
+        pq.pop();
+
+    }
+
+    /*
+
+        ideas: 
+        - union find
+        - topological sort
+        - flood fill algorithm
+        - A* (weighted graph)
+        - Dijkstra's Algorithm
+        - simulation
+
+        approach:
+        - figure out a way to organize the data currently
+        - apply algorithmic approach to the data
+        - create a graphical visual setup to data 
+
+        start = i(0, 0)
+        end =   i(3, 3)
+
+
+        0 1 2 3
+        1 2 3 4
+        2 3 4 5
+        3 4 5 6
+    */
+
+    return 0;
+}
+
 int main() {
     CoorSystem System;
     CoorSystem SystemInit;
@@ -133,7 +208,7 @@ int main() {
 
     System = init_board(
         System,                                                             //! default coordinate system
-        std::vector {14LL, 14LL, 14LL, 14LL},                                           //! default, hard 14 x 14 array 
+        std::vector {3LL, 3LL, 3LL, 3LL},                                           //! default, hard 14 x 14 array 
         std::tuple<bool, ll, std::vector<ll>> {true, 6, std::vector<ll>(0)} //! default, random init with 6 colors
     );
 
@@ -146,6 +221,12 @@ int main() {
     debug_prints(System);
     debug_prints(SystemInit);
 
-    
+    std::cout << "getting: " << System.get({0, 0, 0, 0}) << std::endl;
+    std::cout << "getting: " << System.get({0, 1, 0, 0}) << std::endl;
+    std::cout << "getting: " << System.get({0, 0, 1, 0}) << std::endl;
+    std::cout << "getting: " << System.get({0, 0, 0, 1}) << std::endl;
+    ll ans = solver(System);
+    std::cout << ans << std::endl;
+
     return 0;
 };
